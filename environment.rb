d802223bbe5e7422a90454e6799133ed19a50e9c
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-04-24 21:02:26 -0400

    :memo: typo

--- environment.rb
+++ environment.rb
@@ -4,7 +4,7 @@ Checklist.prepare_load_path
 Checklist.prepare_env
 
 if Checklist.env == :production
-  log_file = File.join(settings.root, "logs", "production.log")
+  log_file = File.join(settings.root, "log", "production.log")
   Checklist.logger = Logger.new(log_file, 10, 1_024_000)
   Checklist.logger = Logger::WARN
 end
